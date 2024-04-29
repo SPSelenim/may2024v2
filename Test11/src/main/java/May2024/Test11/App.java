198d9c61d078b473d2f2ed0dc795c2f53af10ac2
spselenim <pawal.satish@gmail.com>
2024-04-29 18:07:10 +0530

    First

---
+++ Test11/src/main/java/May2024/Test11/App.java
@@ -0,0 +1,13 @@
+package May2024.Test11;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Test11/src/test/java/May2024/Test11/NewTest.java
@@ -0,0 +1,10 @@
+package May2024.Test11;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("Test NG");
+  }
+}
